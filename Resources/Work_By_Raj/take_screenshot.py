fd53ab563586bac7a9add9f484bf84db00be9642
Shaishav Surati <shaishavsurati06@gmail.com>
2021-03-02 10:18:56 +0530

    add file

---
+++ Resources/Work_By_Raj/take_screenshot.py
@@ -0,0 +1,23 @@
+# oMNS
+
+
+import pyautogui
+import datetime
+
+# scrrenshot -> its take a screenshot and save it we provide path as an argument, the file name must required proper format
+
+
+def take_screenshot(cmd):
+    if "screenshot" in cmd or "screen shot" in cmd:
+        if "with name" in cmd:
+            index_of_with_name = cmd.index("with name")
+            index_after_adding_10= index_of_with_name + 10 
+           
+            name = cmd[index_after_adding_10: ]
+            pyautogui.screenshot("E:\\Achintya\\Images\\"+name+".png")
+        
+        else:
+            pyautogui.screenshot("E:\\Achintya\\Images\\"+str(datetime.datetime.now()).replace(":","-")+".png")
+        #TODO: ^^^^ check dirctory of Achintya and then make folder named Image and save photo there
+
+take_screenshot("")
