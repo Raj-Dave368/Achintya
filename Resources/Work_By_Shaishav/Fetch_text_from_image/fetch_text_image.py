b5edde302d16843b44f59dc768172d15c973d044
Raj-Dave368 <rajdaveomns1@gmail.com>
2021-04-09 07:39:21 +0530

    Fetch text from Image COMPLETED

--- Resources/Work_By_Shaishav/Fetch_text_from_image/fetch_text_image.py
+++ Resources/Work_By_Shaishav/Fetch_text_from_image/fetch_text_image.py
@@ -3,19 +3,23 @@
 
 import easyocr
 import tkinter as tk
-
+import pyperclip
+import time
 
 def fetch_text_from_image(cmd: str):
+    from tkinter.filedialog import askopenfilename
+
+
     if 'fetch text from an image' in cmd or 'fetch text from image' in cmd or \
         "get text from an image" in cmd or  "get text from image" in cmd:
         root = tk.Tk()
-
         canvas1 = tk.Canvas(root, width=800, height=300)
         canvas1.pack()
 
+        pathLabel = tk.Label(root, text="Path: ")
+        canvas1.create_window(111, 120, window=pathLabel)
         entry1 = tk.Entry(root)
-        canvas1.create_window(200, 140, window=entry1)
-
+        canvas1.create_window(200, 120, window=entry1)
 
         def get_Path_and_fetch_text():
             img = entry1.get()
@@ -30,15 +34,29 @@ def fetch_text_from_image(cmd: str):
                 text += result[1] + ' '
 
             print(text)
-            label1 = tk.Label(root, text=text)
-            canvas1.create_window(200, 230, window=label1)
+            pyperclip.copy(text)
+            w.insert(1.0, text)
+            label1 = tk.Label(root, text="Text Has Been Copied To the Clipboard")
+            canvas1.create_window(200, 260, window=label1)
 
+        def select_a_file():
+            filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+            print("Selcted File Is: " + filename)
+            entry1.insert(0, filename)
+            time.sleep(1)
+            get_Path_and_fetch_text()
 
-        button1 = tk.Button(text='Enter the Path of Your Image with extension', command=get_Path_and_fetch_text)
-        canvas1.create_window(200, 180, window=button1)
+        w = tk.Text(root, height=15, width=50)
+        canvas1.create_window(568, 140, window=w)
+
+        button1 = tk.Button(text='Select File', command=select_a_file)
+        canvas1.create_window(200, 160, window=button1)
+
+        button2 = tk.Button(text='Enter the Path of Your Image with extension', command=get_Path_and_fetch_text)
+        canvas1.create_window(200, 200, window=button2)
+
 
         root.mainloop()
-
 
 fetch_text_from_image("fetch text from an image")
 
